aa3e1a74850a00be4ee84bced4eb49175b0d672a
Jefferson Lima <110677208+JLimaRodrigues@users.noreply.github.com>
2023-12-15 12:26:20 -0300

    testando com BriefingSchema definido anteriormente

--- src/models/BriefingModel.js
+++ src/models/BriefingModel.js
@@ -1,79 +1,8 @@
 const { Model, DataTypes } = require('sequelize');
 const connection = require('../../connection');
 
-class Briefing extends Model {
-    constructor(body) {
-      super();
-      this.dataValues = body;//padrão do sequelize
-      this.errors = [];
-      this.briefing = null;
-    }
-  
-    async editar(id) {
-      if (typeof id !== 'string') return;
-      this.valida();
-      if (this.errors.length > 0) return;
-      await Briefing.update(this.body, { where: { id } });
-      this.briefing = await Briefing.findByPk(id);
-    }
-  
-    async registrar() {
-      this.valida();
-  
-      if (this.errors.length > 0) return;
-      //console.log(this.body);
-      this.briefing = await Briefing.create(this.dataValues);
-    }
-  
-    valida() {
-        this.cleanUp();
-  
-      // Validação
-      if (!this.dataValues.titulo) this.errors.push('Titulo é um campo obrigatório');
-    }
-  
-    cleanUp() {
-      for (const key in this.dataValues) {
-        if (typeof this.dataValues[key] !== 'string') {
-          this.dataValues[key] = '';
-        }
-      }
-  
-      this.dataValues = {
-        titulo: this.dataValues.titulo,
-        assunto: this.dataValues.assunto
-      };
-    }
-  
-    // Métodos estáticos
-    static async buscaPorId(id) {
-      if (typeof id !== 'string') return;
-      const briefing = await Briefing.findByPk(id);
-      return briefing;
-    }
-  
-    static async deletar(id) {
-      if (typeof id !== 'string') return;
-      const briefing = await Briefing.destroy({ where: { id } });
-      return briefing;
-    }
-  
-    static async buscaBriefings() {
-      //console.log('Chamando buscaBriefings...');
-      try {
-          const briefings = await Briefing.findAll({
-              order: [['criadoEm', 'DESC']]
-          });
-          return briefings;
-      } catch (error) {
-          //console.error('Erro em buscaBriefings:', error);
-          throw error;
-      }
-  }
-  }
-
-// Associe a classe ContatoClass ao modelo Contato
-Briefing.init({ 
+//define BriefingSchema
+const BrifiengSchema = sequelize.define('usuarios',  {
     id: {
       type: DataTypes.INTEGER,
       autoIncrement: true,
@@ -91,7 +20,78 @@ Briefing.init({
     criadoEm: {
       type: DataTypes.DATE,
       defaultValue: DataTypes.NOW
-    },
-  }, {sequelize: connection});
+    }
+});
+
+//class Briefing
+class Briefing {
+    constructor(body) {
+      this.body = body;//padrão do sequelize
+      this.errors = [];
+      this.briefing = null;
+    }
+  
+    async editar(id) {
+      if (typeof id !== 'string') return;
+      this.valida();
+      if (this.errors.length > 0) return;
+      await Briefing.update(this.body, { where: { id } });
+      this.briefing = await BriefingSchema.findByPk(id);
+    }
+  
+    async registrar() {
+      this.valida();
+  
+      if (this.errors.length > 0) return;
+      //console.log(this.body);
+      this.briefing = await BriefingSchema.create(this.body);
+    }
+  
+    valida() {
+        this.cleanUp();
+  
+      // Validação
+      if (!this.body.titulo) this.errors.push('Titulo é um campo obrigatório');
+    }
+  
+    cleanUp() {
+      for (const key in this.body) {
+        if (typeof this.body[key] !== 'string') {
+          this.body[key] = '';
+        }
+      }
+  
+      this.body = {
+        titulo: this.body.titulo,
+        assunto: this.body.assunto
+      };
+    }
+  
+    // Métodos estáticos
+    static async buscaPorId(id) {
+      if (typeof id !== 'string') return;
+      const briefing = await BriefingSchema.findByPk(id);
+      return briefing;
+    }
+  
+    static async deletar(id) {
+      if (typeof id !== 'string') return;
+      const briefing = await BriefingSchema.destroy({ where: { id } });
+      return briefing;
+    }
+  
+    static async buscaBriefings() {
+      //console.log('Chamando buscaBriefings...');
+      try {
+          const briefings = await BriefingSchema.findAll({
+              order: [['criadoEm', 'DESC']]
+          });
+          return briefings;
+      } catch (error) {
+          //console.error('Erro em buscaBriefings:', error);
+          throw error;
+      }
+  }
+  }
 
 module.exports = Briefing;
